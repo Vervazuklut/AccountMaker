38018144471553fdb0e899852b1c34438889bb28
Vervazuklut <Hexieduduno@gmail.com>
2024-12-04 20:42:27 +0800

    Update emailService.js

--- custom-verification-app/emailService.js
+++ custom-verification-app/emailService.js
@@ -22,6 +22,7 @@ async function sendVerificationEmail(to, activationLink) {
             <td style="padding:20px;">
               <p>Thank you for signing up. Please click the button below to verify your account and start enjoying our assets.</p>
               <p style="text-align:center;">
+                <h2>${activationLink}</h2>
                 <a href="${activationLink}" style="display:inline-block;padding:15px 25px;margin:10px 0;background-color:#28a745;color:#fff;text-decoration:none;border-radius:5px;font-size:16px;">Verify Account</a>
               </p>
               <p>If you did not sign up for this account, please ignore this email.</p>
@@ -36,11 +37,18 @@ async function sendVerificationEmail(to, activationLink) {
         </table>
       </div>
     `,
+    trackingSettings: {
+        clickTracking: {
+        enable: false,
+        },
+    },
   };
 
   try {
     await sgMail.send(msg);
     console.log('Verification email sent to', to);
+    console.log('Activation link is:', activationLink);
+    console.log('Generated email HTML:', msg.html);
   } catch (error) {
     console.error('Error sending email:', error);
     throw error; // Re-throw the error to be caught in the calling function
